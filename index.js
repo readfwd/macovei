841b0bff35c055543225b2ccf424753a0b4eb818
Theodor Văraru <theo@vararu.org>
2014-08-15 18:24:25 +0300

    Add mean-seo

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var compression = require('compression');
+var seo = require('mean-seo');
 
 var app = express();
 
 // Enable gzip compression.
 app.use(compression());
+
+// Enable PhantomJS SEO.
+if (process.env.REDISCLOUD_URL) {
+  // If we've got Redis available, use that.
+  app.use(seo({ cacheClient: 'rediscloud' }));
+} else {
+  // Otherwise, use regular disk-based cache.
+  app.use(seo());
+}
 
 app.use(express.static(__dirname + '/dist'));
 
