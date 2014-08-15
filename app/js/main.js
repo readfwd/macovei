330fd3e3947dfccb81fb924aeefe2642663ef4e6
Theodor Văraru <theo@vararu.org>
2014-08-15 18:04:31 +0300

    Add routing.

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,17 @@ var $ = require('./shims/jquery');
 var _ = require('lodash');
 var MainView = require('./views/main');
 var Posts = require('./models/posts-collection');
+var Router = require('./router');
 
 module.exports = {
   launch: _.once(function () {
     var self = window.app = this;
 
+    // Create our global 'posts' object that contains the Facebook feed.
     this.posts = new Posts();
+
+    // Init our URL handlers and the history tracker.
+    this.router = new Router();
 
     $(document).ready(function () {
       // Initialize our main view.
@@ -19,8 +24,18 @@ module.exports = {
 
       // Render it.
       mainView.render();
+
+      // Listen for new pages from the router.
+      self.router.on('newPage', mainView.setPage, mainView);
+
+      // Start our router and show the appropriate page.
+      self.router.history.start({ pushState: true, root: '/' });
     });
-  })
+  }),
+  navigate: function (page) {
+    var url = (page.charAt(0) === '/') ? page.slice(1) : page;
+    this.router.history.navigate(url, { trigger: true });
+  }
 };
 
 module.exports.launch();
--- app/js/views/main.js
+++ app/js/views/main.js
@@ -3,18 +3,28 @@
 /* global app */
 var Backbone = require('../shims/backbone');
 var View = Backbone.View;
+var _ = require('lodash');
+var ViewSwitcher = require('ampersand-view-switcher');
 var templates = require('../lib/templates');
-var PostsView = require('./posts');
 
 module.exports = View.extend({
   template: templates.body,
   render: function () {
     this.$el.html(this.template());
 
-    this.postsView = new PostsView({
-      collection: app.posts,
-      el: this.$('[role="posts-collection"]')
+    // Init and configure the page switcher.
+    this.pageSwitcher = new ViewSwitcher(this.$('[role="page-container"]')[0], {
+      show: function (newView) {
+        document.title = _.result(newView.pageTitle) || 'Monica Macovei';
+        document.scrollTop = 0;
+
+        app.currentPage = newView;
+      }
     });
+
     return this;
+  },
+  setPage: function (view) {
+    this.pageSwitcher.set(view);
   }
 });
