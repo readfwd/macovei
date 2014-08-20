0b05f8c2ac7b85056ad66b983b90419bfb2605da
Theodor Văraru <theo@vararu.org>
2014-08-20 21:55:37 +0300

    Fiddle with templates.

--- app/js/views/main.js
+++ app/js/views/main.js
@@ -3,7 +3,6 @@
 /* global app */
 var Backbone = require('../shims/backbone');
 var View = Backbone.View;
-var _ = require('lodash');
 var $ = require('../shims/jquery');
 var ViewSwitcher = require('ampersand-view-switcher');
 var templates = require('../lib/templates');
@@ -19,7 +18,7 @@ module.exports = View.extend({
     // Init and configure the page switcher.
     this.pageSwitcher = new ViewSwitcher(this.$('[role="page-container"]')[0], {
       show: function (newView) {
-        document.title = _.result(newView.pageTitle) || 'Monica Macovei';
+        document.title = newView.pageTitle || 'Monica Macovei';
         document.scrollTop = 0;
 
         app.currentPage = newView;
