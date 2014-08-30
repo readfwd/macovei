87eaff01b933d5661559c0590afe5255f0249d8d
Paul Balogh <paul.balogh@gmail.com>
2014-08-30 15:55:42 +0300

    404 message

---
+++ app/js/pages/eroare404.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Backbone = require('../shims/backbone');
+var View = Backbone.View;
+var templates = require('../lib/templates');
+
+module.exports = View.extend({
+  pageTitle: 'Monica Macovei Presedinte | eroare 404',
+  template: templates.pages.eroare404,
+  render: function () {
+    this.$el.html(this.template());
+    return this;
+  }
+});
