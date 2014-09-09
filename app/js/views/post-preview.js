3665578d6432efa18766c05b1afd5f83889e80e0
Paul Balogh <paul.balogh@gmail.com>
2014-09-09 23:09:59 +0300

    doina jela - contre tout espoir

--- app/js/views/post-preview.js
+++ app/js/views/post-preview.js
@@ -6,12 +6,14 @@ var templates = require('../lib/templates');
 var posts = require('../lib/posts-json.json');
 var moment = require('moment/min/moment-with-locales');
 moment.locale('ro');
+console.log(moment());
 
 
 module.exports = window.postView = View.extend({
   template: templates.includes.postPreview,
 
   render: function () {
+    // console.log(posts[this.model.attributes.slug].date),
     this.$el.html(this.template({
       title: posts[this.model.attributes.slug].title,
       preview: posts[this.model.attributes.slug].preview,
@@ -19,7 +21,7 @@ module.exports = window.postView = View.extend({
       thumb: posts[this.model.attributes.slug].thumb,
       author: posts[this.model.attributes.slug].author,
       source: posts[this.model.attributes.slug].source,
-      date: moment(posts[this.model.attributes.slug].date).fromNow(),
+      date: moment(posts[this.model.attributes.slug].date, 'YYYY-MM-DD').fromNow(),
     }));
     return this;
   }
