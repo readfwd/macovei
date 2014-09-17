34c99cb4deb39cb9cf44ec3c791867eebb429719
Marius Petcu <dapetcu21@gmail.com>
2014-09-17 16:35:40 +0300

    Moved build to slug compilation

---
+++ postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./node_modules/bower/bin/bower install
+if [ "$NODE_ENV" == "production" ]; then
+    ./node_modules/gulp/bin/gulp.js build:dist
+    ./node_modules/gulp/bin/gulp.js clean
+    mv ./app/js/lib/routes.json out-routes.json
+    mv ./app/js/lib/posts-json.json out-posts-json.json
+    rm -rf ./app ./test ./gulp
+    mkdir -p ./app/js/lib
+    mv out-routes.json ./app/js/lib/routes.json
+    mv out-posts-json.json ./app/js/lib/posts-json.json
+fi
