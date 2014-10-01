2b578a6ea2326d85f66cb316249ae225d3bf1ae4
Andrei Constantinescu <andrei.c@icloud.com>
2014-10-01 19:11:57 +0300

    JSONify reasons page. Add share on facebook button.

--- app/js/pages/motive-sa-votezi-macovei.js
+++ app/js/pages/motive-sa-votezi-macovei.js
@@ -3,12 +3,22 @@
 var Backbone = require('../shims/backbone');
 var View = Backbone.View;
 var templates = require('../lib/templates');
+var reasons = require('../lib/reasons-to-vote');
+var _ = require('lodash');
+var urlrepl = require('../lib/url-replace');
 
 module.exports = View.extend({
   pageTitle: 'Monica Macovei Presedinte | 20 de motive să votezi Macovei',
   template: templates.pages.motiveSaVoteziMacovei,
   render: function () {
-    this.$el.html(this.template());
+    _.forEach(reasons, function (reason) {
+      // console.log(reason);
+      reason.image = window.location.origin + urlrepl(reason.image);
+      console.log(reason.image);
+    });
+    this.$el.html(this.template({
+      reasons: reasons
+    }));
     return this;
   }
 });
