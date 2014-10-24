7392b84a3f2158066f0ce57689c1436137639338
Andrei Constantinescu <andrei.c@icloud.com>
2014-10-24 11:00:35 +0300

    Finish POST request.

--- app/js/pages/scrisoare.js
+++ app/js/pages/scrisoare.js
@@ -3,12 +3,41 @@
 var Backbone = require('../shims/backbone');
 var View = Backbone.View;
 var templates = require('../lib/templates');
+var $ = require('../shims/jquery');
 
 module.exports = View.extend({
   pageTitle: 'Monica Macovei Presedinte | Scrisoare',
   template: templates.pages.scrisoare,
+
+  events: {
+    'click .sendIt': 'sendLetter'
+  },
+
   render: function () {
     this.$el.html(this.template());
     return this;
+  },
+
+  sendLetter: function () {
+    var emailRegxp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    var letterData = {
+      title: 1,
+      destination: 1,
+      sendTo: "add@a.cp"
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/send',
+      data: JSON.stringify(letterData),
+      contentType: 'Application/json',
+      success: function() {
+        console.log("sent");
+      },
+      error: function () {
+        console.log('error');
+      }
+    })
   }
 });
